3dc36b1362ae3dbd86482cbc5e85093308d4277b
sangnopro2k4 <tretrausang321@gmail.com>
2023-11-05 21:24:18 +0700

    Click on the mic to check the product

--- app/src/main/java/com/example/aexpress/activities/MainActivity.java
+++ app/src/main/java/com/example/aexpress/activities/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.aexpress.activities;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.util.Log;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -64,7 +72,9 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onButtonClicked(int buttonCode) {
-
+                if (buttonCode == MaterialSearchBar.BUTTON_SPEECH) {
+                    startTextFromSpeech();
+                }
             }
         });
 
@@ -199,5 +209,35 @@ public class MainActivity extends AppCompatActivity {
         binding.productList.setAdapter(productAdapter);
     }
 
+    private void startTextFromSpeech() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hãy nói gì đó...");
 
+        try {
+            myLauncher.launch(intent);
+        } catch (ActivityNotFoundException e) {
+            // Xử lý nếu không tìm thấy ứng dụng nhận dạng giọng nói trên thiết bị
+            Toast.makeText(this, "Device is not supported", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    ActivityResultLauncher<Intent> myLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            ArrayList<String> resultText = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                            if (resultText != null && resultText.size() > 0) {
+                                String spokenText = resultText.get(0);
+                                // Xử lý văn bản được nhận dạng (làm chức năng tìm kiếm)
+                                binding.searchBar.openSearch();
+                                binding.searchBar.setText(spokenText);
+                            }
+                        }
+                    }
+                }
+            });
 }
