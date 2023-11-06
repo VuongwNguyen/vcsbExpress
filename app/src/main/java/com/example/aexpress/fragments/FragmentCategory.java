dee21b6e74233bc43b9103ce3ad32beb9aa1c6c9
nguyennambac <gokuvtru7@gmail.com>
2023-11-06 22:23:16 +0700

    Them fragments

---
+++ app/src/main/java/com/example/aexpress/fragments/FragmentCategory.java
@@ -0,0 +1,112 @@
+package com.example.aexpress.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.aexpress.R;
+import com.example.aexpress.adapters.CategoryAdapter;
+import com.example.aexpress.databinding.FragmentCategoryBinding;
+import com.example.aexpress.model.Category;
+import com.example.aexpress.utils.Constants;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class FragmentCategory extends Fragment {
+    private FragmentCategoryBinding binding;
+    CategoryAdapter categoryAdapter;
+    ArrayList<Category> categories;
+
+    public FragmentCategory() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentCategoryBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initCategories();
+    }
+
+    void initCategories() {
+        categories = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(getContext(), categories);
+
+        getCategories();
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 4);
+        binding.categoriesList.setLayoutManager(layoutManager);
+        binding.categoriesList.setAdapter(categoryAdapter);
+    }
+
+    void getCategories() {
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+
+        StringRequest request = new StringRequest(Request.Method.GET, Constants.GET_CATEGORIES_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    Log.e("err", response);
+                    JSONObject mainObj = new JSONObject(response);
+                    if(mainObj.getString("status").equals("success")) {
+                        JSONArray categoriesArray = mainObj.getJSONArray("categories");
+                        for(int i =0; i< categoriesArray.length(); i++) {
+                            JSONObject object = categoriesArray.getJSONObject(i);
+                            Category category = new Category(
+                                    object.getString("name"),
+                                    Constants.CATEGORIES_IMAGE_URL + object.getString("icon"),
+                                    object.getString("color"),
+                                    object.getString("brief"),
+                                    object.getInt("id")
+                            );
+                            categories.add(category);
+                        }
+                        categoryAdapter.notifyDataSetChanged();
+                    } else {
+                        // DO nothing
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(request);
+    }
+}
---
+++ app/src/main/java/com/example/aexpress/fragments/FragmentHome.java
@@ -0,0 +1,134 @@
+package com.example.aexpress.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.aexpress.R;
+import com.example.aexpress.adapters.ProductAdapter;
+import com.example.aexpress.databinding.FragmentHomeBinding;
+import com.example.aexpress.model.Product;
+import com.example.aexpress.utils.Constants;
+
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class FragmentHome extends Fragment {
+    private FragmentHomeBinding binding;
+    ProductAdapter productAdapter;
+    ArrayList<Product> products;
+
+
+    public FragmentHome() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initProducts();
+        initSlider();
+    }
+
+    private void initSlider() {
+        getRecentOffers();
+    }
+
+    void getRecentProducts() {
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+
+        String url = Constants.GET_PRODUCTS_URL + "?count=8";
+        StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
+            try {
+                JSONObject object = new JSONObject(response);
+                if(object.getString("status").equals("success")){
+                    JSONArray productsArray = object.getJSONArray("products");
+                    for(int i =0; i< productsArray.length(); i++) {
+                        JSONObject childObj = productsArray.getJSONObject(i);
+                        Product product = new Product(
+                                childObj.getString("name"),
+                                Constants.PRODUCTS_IMAGE_URL + childObj.getString("image"),
+                                childObj.getString("status"),
+                                childObj.getDouble("price"),
+                                childObj.getDouble("price_discount"),
+                                childObj.getInt("stock"),
+                                childObj.getInt("id")
+
+                        );
+                        products.add(product);
+                    }
+                    productAdapter.notifyDataSetChanged();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> { });
+
+        queue.add(request);
+    }
+
+    void getRecentOffers() {
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+
+        StringRequest request = new StringRequest(Request.Method.GET, Constants.GET_OFFERS_URL, response -> {
+            try {
+                JSONObject object = new JSONObject(response);
+                if(object.getString("status").equals("success")) {
+                    JSONArray offerArray = object.getJSONArray("news_infos");
+                    for(int i =0; i < offerArray.length(); i++) {
+                        JSONObject childObj =  offerArray.getJSONObject(i);
+                        binding.carousel.addData(
+                                new CarouselItem(
+                                        Constants.NEWS_IMAGE_URL + childObj.getString("image"),
+                                        childObj.getString("title")
+                                )
+                        );
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {});
+        queue.add(request);
+    }
+
+    void initProducts() {
+        products = new ArrayList<>();
+        productAdapter = new ProductAdapter(getContext(), products);
+
+        getRecentProducts();
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        binding.productList.setLayoutManager(layoutManager);
+        binding.productList.setAdapter(productAdapter);
+    }
+}
